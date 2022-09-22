81ef721d6ed14d1848e8e129fd2fba73c9b08d51
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-22 20:46:02 -0300

    finalizado-exercicio-06

---
+++ ex06/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+const increment = (number, value) => number + value; 
+
+// Only change code above this line
+console.log(increment(5, 2)); //7
+console.log(increment(5)); //6
+
+/*Colocando o número 1 em value, ele vai acabar somando o 1 com o 5
+que logo vai dar 6, se eu colocar o 2 no lugar que vou passar o value, 
+o valor só vai ser substituído  */
+
+
+/*EXAMPLE
+
+const greeting = (name = "Anonymous") => "Hello " + name;
+
+console.log(greeting("John"));
+console.log(greeting())
+*/
