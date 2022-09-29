917574876485e1744f06697085c7d4efac116c94
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-29 20:51:02 -0300

    correção

--- ex14/index.js
+++ ex14/index.js
@@ -11,3 +11,7 @@ const half = ({max, min}) => (max + min) / 2.0;
 // Only change code above this line
 
 console.log(half(stats)); //28.015
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2a40e017fa4d274ee17f6364db6c2cb0240ad658
