86117596042e136e68fee9fc69ee134581efe3de
Tales gomes <tales.gomes_sl@hotmail.com>
2022-10-04 20:12:07 -0300

    finalizado-exercicio-22

---
+++ ex22/index.js
@@ -0,0 +1,9 @@
+import { uppercaseString, lowercaseString } from './string_functions';
+
+// Only change code above this line]
+
+uppercaseString("hello"); //HELLO
+lowercaseString("WORLD!")//world!
+
+
+/*Importando as funções que foram feitas no meu projeto anterior */
