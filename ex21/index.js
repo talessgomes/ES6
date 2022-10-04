68737711a7d229f9cd2c019bc5b334cddcfd4721
Tales gomes <tales.gomes_sl@hotmail.com>
2022-10-04 19:59:43 -0300

    finalizado-exercicio-21

---
+++ ex21/index.js
@@ -0,0 +1,13 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowerCaseString = (string) => {
+    return string.toLowerCase()
+}
+
+
+export {uppercaseString, lowerCaseString};
+
+/*Neste projeto eu vou estar exportando duas funções
+ */
