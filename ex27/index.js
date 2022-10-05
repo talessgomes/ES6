b47db6212933ba2a13f159a1a7585ae5eeae4f4a
Tales gomes <tales.gomes_sl@hotmail.com>
2022-10-04 21:02:43 -0300

    finalizado-exercicio-27

---
+++ ex27/index.js
@@ -0,0 +1,23 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        // Change this line
+        resolve("We got the data");
+    } else {
+        // Change this line
+        reject("Data not received");
+    }
+});
+
+/* EXAMPLE
+
+const myPromise = new Promise((resolve, reject) => {
+  if(condition here) {
+    resolve("Promise was fulfilled");
+  } else {
+    reject("Promise was rejected");
+  }
+});
+ */
